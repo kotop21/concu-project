b8d7e2345efc0b54671f4b36b7c9fcbba05224a5
Victor <72082776+kotop21@users.noreply.github.com>
2024-02-13 11:19:06 +0000

    modified:   src/modules/home.vue 	modified:   src/pages/index.astro 	modified:   src/styles/globals.css

--- src/modules/home.vue
+++ src/modules/home.vue
@@ -84,7 +84,7 @@ onBeforeUnmount(() => {
       <Dialog>
         <DialogTrigger>
           <Button>
-            <ScanEye />Перегляд моделі в 3д
+            <ScanEye />Вояджер в 3д
           </Button>
         </DialogTrigger>
         <DialogContent>
